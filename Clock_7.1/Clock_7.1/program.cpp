6f226431f64deda71a1889f3cc11d78fd142f232
Alex <ildarkin.alexey@gmail.com>
2015-11-21 21:12:50 +0300

    Upload 7.1.

--- Clock_7.1/Clock_7.1/program.cpp
+++ Clock_7.1/Clock_7.1/program.cpp
@@ -1,57 +1,75 @@
 #include "program.h"
 
-void PollEvent(Program &program, Event &event) {
-	while (program.window.pollEvent(event))
-	{
-		if (event.type == sf::Event::Closed)
-			program.window.close();
-	}
+void RotationHands(ClockElements &clock) {
+	clock.secondHand.setRotation(DEGREES_IN_CIRCLE / COUNT_TIME_UNITS * float(clock.time.systemTime.wSecond) - 90);
+	clock.minuteHand.setRotation((DEGREES_IN_CIRCLE / COUNT_TIME_UNITS * float(clock.time.systemTime.wSecond) / COUNT_TIME_UNITS) + float(clock.time.systemTime.wMinute) * DEGREES_IN_CIRCLE / COUNT_TIME_UNITS - DEGREES_IN_CIRCLE / 4);
+	clock.hourHand.setRotation(DEGREES_IN_CIRCLE / COUNT_TIME_UNITS * COUNT_TIME_UNITS / CORRECTION_UNIT * float(clock.time.systemTime.wMinute) / COUNT_TIME_UNITS + float(clock.time.systemTime.wHour) * DEGREES_IN_CIRCLE / ELEMENTS_ON_CLOCK_FACE - DEGREES_IN_CIRCLE / 4);
 }
 
-void CalculateDivisionPos(float rotation, ClockElements &clock) {
-	double x = clock.hourHand.getPosition().x + (RADIUS_CLOCK_FACE - 3.5)*cos(rotation * 3.14159265 / 180);
-	double y = clock.hourHand.getPosition().y + (RADIUS_CLOCK_FACE - 3.5)*sin(rotation * 3.14159265 / 180);
-	clock.hourDivision.setPosition(float(x), float(y));
+void UpdateTime(ClockElements &clock) {
+	GetLocalTime(&clock.time.systemTime);
+	RotationHands(clock);
+}
+
+void CalculateElementPos(float angle, ClockElements &clock) {
+	double x = clock.hourHand.getPosition().x - CORRECTION_UNIT / 2 + (RADIUS_CLOCK_FACE - CORRECTION_UNIT)*cos(angle * PI / (DEGREES_IN_CIRCLE / 2));
+	double y = clock.hourHand.getPosition().y - CORRECTION_UNIT + (RADIUS_CLOCK_FACE - CORRECTION_UNIT)*sin(angle * PI / (DEGREES_IN_CIRCLE / 2));
+	clock.hour.setPosition(float(x), float(y));
 }
 
 void DrawObjects(Program &program, ClockElements &clock) {
 	program.window.clear(Color::White);
 	program.window.draw(clock.clockFace);
+	UpdateTime(clock);
+	int hour = 1;
+	for (float angle = 0; angle < DEGREES_IN_CIRCLE; angle += 30) {
+		hour++;
+		if (hour == ELEMENTS_ON_CLOCK_FACE) {
+			hour = 0;
+		}
+		clock.hour.setString(to_string(hour + 1));
+		CalculateElementPos(angle, clock);
+		program.window.draw(clock.hour);
+	}
 	program.window.draw(clock.hourHand);
 	program.window.draw(clock.minuteHand);
 	program.window.draw(clock.secondHand);
-
-	//std::cout << sin(30 * 3.14 / 180);
-	for (float angle = 0; angle < 360; angle += 30) {
-		CalculateDivisionPos(angle, clock);
-		program.window.draw(clock.hourDivision);
-	}
-
+	program.window.draw(clock.clockCore);
 	program.window.display();
 }
 
 void CreateObjects(Program &program, ClockElements &clock){
-	program.window.create(VideoMode(WIDTH_WINDOW, HEIGHT_WINDOW), "Sorry, but while you can't check the current time. :c");
+	program.window.create(VideoMode(WIDTH_WINDOW, HEIGHT_WINDOW), "Now you can check the current time. c:");
 	clock.clockFace.setRadius(RADIUS_CLOCK_FACE);
 	clock.clockFace.setFillColor(Color::White);
-	clock.clockFace.setOutlineThickness(2);
+	clock.clockFace.setOutlineThickness(3);
 	clock.clockFace.setOutlineColor(Color::Black);
 	clock.hourHand.setSize(Vector2f(WIDTH_HOUR_HAND, HEIGHT_HOUR_HAND));
 	clock.hourHand.setFillColor(Color::Black);
-	clock.hourHand.setPosition(clock.clockFace.getPosition().x + RADIUS_CLOCK_FACE, clock.clockFace.getPosition().y + RADIUS_CLOCK_FACE);
+	clock.hourHand.setPosition(clock.clockFace.getPosition().x + RADIUS_CLOCK_FACE, clock.clockFace.getPosition().y + RADIUS_CLOCK_FACE + HEIGHT_HOUR_HAND / 2);
 	clock.minuteHand.setSize(Vector2f(WIDTH_MINUTE_HAND, HEIGHT_MINUTE_HAND));
 	clock.minuteHand.setFillColor(Color::Black);
 	clock.minuteHand.setPosition(clock.clockFace.getPosition().x + RADIUS_CLOCK_FACE, clock.clockFace.getPosition().y + RADIUS_CLOCK_FACE);
-	clock.minuteHand.setRotation(30); //TODO
 	clock.secondHand.setSize(Vector2f(WIDTH_SECOND_HAND, HEIGHT_SECOND_HAND));
 	clock.secondHand.setFillColor(Color::Red);
 	clock.secondHand.setPosition(clock.clockFace.getPosition().x + RADIUS_CLOCK_FACE, clock.clockFace.getPosition().y + RADIUS_CLOCK_FACE);
-	clock.secondHand.setRotation(60); //TODO
-	clock.hourDivision.setRadius(RADIUS_HOUR_DIVISION);
-	clock.hourDivision.setFillColor(Color::Black);
-	clock.hourDivision.setPosition(clock.clockFace.getPosition().x + RADIUS_CLOCK_FACE, clock.clockFace.getPosition().y);
+	clock.clockCore.setRadius(RADIUS_HOUR_CORE);
+	clock.clockCore.setFillColor(Color::Black);
+	clock.clockCore.setOrigin(clock.clockCore.getGlobalBounds().width / 2, clock.clockCore.getGlobalBounds().height / 2);
+	clock.clockCore.setPosition(clock.clockFace.getPosition().x + RADIUS_CLOCK_FACE, clock.clockFace.getPosition().y + RADIUS_CLOCK_FACE);
+	clock.font.loadFromFile("CyrilicOld.ttf");
+	clock.hour.setFont(clock.font);
+	clock.hour.setColor(Color::Black);
+	clock.hour.setCharacterSize(FONT_SIZE);
+	clock.hour.setOrigin(clock.hour.getGlobalBounds().width / 2, clock.hour.getGlobalBounds().height / 2);
 }
 
+void PollEvent(Program &program, Event &event) {
+	while (program.window.pollEvent(event)) {
+		if (event.type == sf::Event::Closed)
+			program.window.close();
+	}
+}
 
 void IsOpen(Program &program, ClockElements &clock) {
 	while (program.window.isOpen())
--- Clock_7.1/Clock_7.1/program.h
+++ Clock_7.1/Clock_7.1/program.h
@@ -1,5 +1,9 @@
 #pragma once
+
+
 #include <SFML/Graphics.hpp>
+#include <Windows.h>
+
 
 static const unsigned int WIDTH_WINDOW = 200;
 static const unsigned int HEIGHT_WINDOW = 200;
@@ -10,13 +14,25 @@ static const float WIDTH_MINUTE_HAND = 75;
 static const float HEIGHT_MINUTE_HAND = 3;
 static const float WIDTH_SECOND_HAND = 95;
 static const float HEIGHT_SECOND_HAND = 2;
-static const float RADIUS_HOUR_DIVISION = 1.9f;
+static const float RADIUS_HOUR_CORE = 5;
+static const float DEGREES_IN_CIRCLE = 360;
+static const float COUNT_TIME_UNITS = 60;
+static const float ELEMENTS_ON_CLOCK_FACE = 12;
+static const float CORRECTION_UNIT = 10;
+static const double PI = 3.14159265;
+static const unsigned int FONT_SIZE = 14;
+
 
 using namespace sf;
 using namespace std;
 
+
 struct Program {
 	RenderWindow window;
+};
+
+struct ClockSynchronization {
+	SYSTEMTIME systemTime;
 };
 
 struct ClockElements {
@@ -24,7 +40,10 @@ struct ClockElements {
 	RectangleShape hourHand;
 	RectangleShape minuteHand;
 	RectangleShape secondHand;
-	CircleShape hourDivision;
+	CircleShape clockCore;
+	Font font;
+	Text hour;
+	ClockSynchronization time;
 };
 
 void ActionProgram(Program &program, ClockElements &clock);
