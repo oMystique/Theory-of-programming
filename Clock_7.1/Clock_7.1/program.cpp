e57cb712e37103f0cb917ee05f5132232957f30c
Alex <ildarkin.alexey@gmail.com>
2015-11-21 17:34:50 +0300

    Добавлены элементы циферблата

--- Clock_7.1/Clock_7.1/program.cpp
+++ Clock_7.1/Clock_7.1/program.cpp
@@ -8,12 +8,25 @@ void PollEvent(Program &program, Event &event) {
 	}
 }
 
+void CalculateDivisionPos(float rotation, ClockElements &clock) {
+	double x = clock.hourHand.getPosition().x + (RADIUS_CLOCK_FACE - 3.5)*cos(rotation * 3.14159265 / 180);
+	double y = clock.hourHand.getPosition().y + (RADIUS_CLOCK_FACE - 3.5)*sin(rotation * 3.14159265 / 180);
+	clock.hourDivision.setPosition(float(x), float(y));
+}
+
 void DrawObjects(Program &program, ClockElements &clock) {
 	program.window.clear(Color::White);
 	program.window.draw(clock.clockFace);
 	program.window.draw(clock.hourHand);
 	program.window.draw(clock.minuteHand);
 	program.window.draw(clock.secondHand);
+
+	//std::cout << sin(30 * 3.14 / 180);
+	for (float angle = 0; angle < 360; angle += 30) {
+		CalculateDivisionPos(angle, clock);
+		program.window.draw(clock.hourDivision);
+	}
+
 	program.window.display();
 }
 
@@ -34,6 +47,9 @@ void CreateObjects(Program &program, ClockElements &clock){
 	clock.secondHand.setFillColor(Color::Red);
 	clock.secondHand.setPosition(clock.clockFace.getPosition().x + RADIUS_CLOCK_FACE, clock.clockFace.getPosition().y + RADIUS_CLOCK_FACE);
 	clock.secondHand.setRotation(60); //TODO
+	clock.hourDivision.setRadius(RADIUS_HOUR_DIVISION);
+	clock.hourDivision.setFillColor(Color::Black);
+	clock.hourDivision.setPosition(clock.clockFace.getPosition().x + RADIUS_CLOCK_FACE, clock.clockFace.getPosition().y);
 }
 
 
--- Clock_7.1/Clock_7.1/program.h
+++ Clock_7.1/Clock_7.1/program.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-static const float WIDTH_WINDOW = 200;
-static const float HEIGHT_WINDOW = 200;
+static const unsigned int WIDTH_WINDOW = 200;
+static const unsigned int HEIGHT_WINDOW = 200;
 static const float RADIUS_CLOCK_FACE = 100;
 static const float WIDTH_HOUR_HAND = 65;
 static const float HEIGHT_HOUR_HAND = 5;
@@ -10,6 +10,7 @@ static const float WIDTH_MINUTE_HAND = 75;
 static const float HEIGHT_MINUTE_HAND = 3;
 static const float WIDTH_SECOND_HAND = 95;
 static const float HEIGHT_SECOND_HAND = 2;
+static const float RADIUS_HOUR_DIVISION = 1.9f;
 
 using namespace sf;
 using namespace std;
@@ -23,6 +24,7 @@ struct ClockElements {
 	RectangleShape hourHand;
 	RectangleShape minuteHand;
 	RectangleShape secondHand;
+	CircleShape hourDivision;
 };
 
 void ActionProgram(Program &program, ClockElements &clock);
